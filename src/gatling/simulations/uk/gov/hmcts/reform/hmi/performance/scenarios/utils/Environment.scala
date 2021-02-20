72c1ade48cd930738025222e9cd69e9aaa85b80f
amitHMCTS <amit.sinha@HMCTS.NET>
2021-02-20 01:20:40 +0000

    Update Environment.scala

--- src/gatling/simulations/uk/gov/hmcts/reform/hmi/performance/scenarios/utils/Environment.scala
+++ src/gatling/simulations/uk/gov/hmcts/reform/hmi/performance/scenarios/utils/Environment.scala
@@ -2,9 +2,9 @@ package uk.gov.hmcts.reform.hmi.performance.scenarios.utils
 
 object Environment {
 
-  val baseURL = ""
+  val baseURL = "https://hmi-apim.staging.platform.hmcts.net/hmi"
  val headers = Map(
-    "Ocp-Apim-Subscription-Key" -> "",
+    "Ocp-Apim-Subscription-Key" -> "ecea5d25107e4348b3e3c2c9ee12e023",
     "Source-System"             -> "CFT",
     "Request-Type"              -> "THEFT",
     "Request-Processed-At"      -> "2020-11-18T20:20:39+00:00",
